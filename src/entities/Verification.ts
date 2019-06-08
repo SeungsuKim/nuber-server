70178ca2b707461de8115d3e6b82a0acd353fff4
seungsukim <seungsu0407@gmail.com>
2019-06-08 14:33:25 +0900

    #1.18 Using Types on the Entities

--- src/entities/Verification.ts
+++ src/entities/Verification.ts
@@ -7,13 +7,15 @@ import {
   UpdateDateColumn
 } from "typeorm";
 
+import { verificationTarget } from "../types/types";
+
 @Entity()
 class Verification extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "text" })
-  target: string;
+  @Column({ type: "text", enum: ["PHONE", "EMAIL"] })
+  target: verificationTarget;
 
   @Column({ type: "text" })
   payload: string;
