484f0c733777641bb9a069aa15919192a3789916
seungsukim <seungsu0407@gmail.com>
2019-06-08 23:44:13 +0900

    #1.39 CompletePhoneVerification

---
+++ src/api/User/CompletePhoneVerification/CompletePhoneVerification.resolvers.ts
@@ -0,0 +1,64 @@
+import {
+  CompletePhoneVerificationMutationArgs,
+  CompletePhoneVerificationResponse
+} from "src/types/graphql";
+import { Resolvers } from "src/types/resolvers";
+
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    CompletePhoneVerification: async (
+      _,
+      { phoneNumber, key }: CompletePhoneVerificationMutationArgs
+    ): Promise<CompletePhoneVerificationResponse> => {
+      try {
+        const verification = await Verification.findOne({
+          payload: phoneNumber,
+          key
+        });
+        if (!verification) {
+          return {
+            ok: false,
+            error: "Verification token is not valid",
+            token: null
+          };
+        }
+        verification.verified = true;
+        verification.save();
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+      try {
+        const user = await User.findOne({ phoneNumber });
+        if (user) {
+          user.verifiedPhoneNumber = true;
+          user.save();
+          return {
+            ok: true,
+            error: null,
+            token: "Coming soon"
+          };
+        }
+        return {
+          ok: true,
+          error: null,
+          token: null
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
