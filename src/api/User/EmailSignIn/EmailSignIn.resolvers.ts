15798d33d1b95e02c8b0a3dbc913adadfd84187b
seungsukim <seungsu0407@gmail.com>
2019-06-08 20:41:47 +0900

    #1.33 EmailSignIn Resolver

--- src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -1,27 +1,35 @@
-import User from "src/entities/User";
 import { EmailSiginInResponse, EmailSignInMutationArgs } from "src/types/graphql";
 import { Resolvers } from "src/types/resolvers";
+
+import User from "../../../entities/User";
 
 const resolvers: Resolvers = {
   Mutation: {
     EmailSignIn: async (
       _,
-      args: EmailSignInMutationArgs
-    ): EmailSiginInResponse => {
-      const { email } = args;
+      { email, password }: EmailSignInMutationArgs
+    ): Promise<EmailSiginInResponse> => {
       try {
         const user = await User.findOne({ email });
         if (!user) {
           return {
             ok: false,
             error: "No user found with the email",
-            toekn: null
+            token: null
           };
         }
-
+        const checkPassword = await user.comparePassword(password);
+        if (checkPassword) {
+          return {
+            ok: true,
+            error: null,
+            token: "Comming soon"
+          };
+        }
         return {
           ok: false,
-          error: "해당 이메일"
+          error: "Wrong password",
+          token: null
         };
       } catch (error) {
         return {
