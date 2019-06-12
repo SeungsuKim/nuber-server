c4e73344e452c0f579ffe9bc66637da916325fa7
seungsukim <seungsu0407@gmail.com>
2019-06-13 01:29:25 +0900

    #1.55 Testing Email Verification Resolvers

--- src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -11,33 +11,39 @@ const resolvers: Resolvers = {
     RequestEmailVerification: privateResolver(
       async (_, __, { req }): Promise<RequestEmailVerificationResponse> => {
         const user: User = req.user;
-        if (user.email) {
-          try {
-            const oldVerification = await Verification.findOne({
-              payload: user.email
-            });
-            if (oldVerification) {
-              oldVerification.remove();
+        if (user.email && !user.verifiedEmail) {
+          if (user.email) {
+            try {
+              const oldVerification = await Verification.findOne({
+                payload: user.email
+              });
+              if (oldVerification) {
+                oldVerification.remove();
+              }
+              const newVerification = await Verification.create({
+                payload: user.email,
+                target: "EMAIL"
+              }).save();
+              await sendVerificationEmail(user.fullName, newVerification.key);
+              return {
+                ok: true,
+                error: null
+              };
+            } catch (error) {
+              return {
+                ok: false,
+                error: error.message
+              };
             }
-            const newVerification = await Verification.create({
-              payload: user.email,
-              target: "EMAIL"
-            }).save();
-            await sendVerificationEmail(user.fullName, newVerification.key);
-            return {
-              ok: true,
-              error: null
-            };
-          } catch (error) {
-            return {
-              ok: false,
-              error: error.message
-            };
           }
+          return {
+            ok: false,
+            error: "Your user has no email to be verified"
+          };
         }
         return {
           ok: false,
-          error: "Your user has no email to be verified"
+          error: "No eamil to verfied or already verified"
         };
       }
     )
