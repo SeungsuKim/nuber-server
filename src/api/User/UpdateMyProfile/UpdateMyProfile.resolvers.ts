7c4390a98b8ff87855860133b51ba7487cbd7f5a
seungsukim <seungsu0407@gmail.com>
2019-06-13 02:22:59 +0900

    #1.57 UpdateMyProfile Resolver

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,4 +1,4 @@
-import { UpdateMyProfileMutationArgs } from "src/types/graphql";
+import { UpdateMyProfileMutationArgs, UpdateMyProfileResponse } from "src/types/graphql";
 import { Resolvers } from "src/types/resolvers";
 
 import User from "../../../entities/User";
@@ -7,10 +7,33 @@ import { privateResolver } from "../../../middlewares";
 const resolvers: Resolvers = {
   Mutation: {
     UpdateMyProfile: privateResolver(
-      async (_, args: UpdateMyProfileMutationArgs, { req }) => {
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
         const user: User = req.user;
-        await User.update({ id: user.id }, {});
+        const notNullArgs = {};
+        Object.keys(args).forEach(key => {
+          if (args[key] !== null) {
+            notNullArgs[key] = args[key];
+          }
+        });
+        try {
+          await User.update({ id: user.id }, { ...notNullArgs });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
       }
     )
   }
 };
+
+export default resolvers;
