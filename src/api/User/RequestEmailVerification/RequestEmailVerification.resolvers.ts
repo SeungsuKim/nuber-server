6f1408a43c2652a0bade44f238192a066c1dbd0e
seungsukim <seungsu0407@gmail.com>
2019-06-13 00:58:44 +0900

    #1.53 RequestEmailVerification Resolver

---
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -0,0 +1,47 @@
+import { RequestEmailVerificationResponse } from "src/types/graphql";
+import { Resolvers } from "src/types/resolvers";
+
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import { privateResolver } from "../../../middlewares";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    RequestEmailVerification: privateResolver(
+      async (_, __, { req }): Promise<RequestEmailVerificationResponse> => {
+        const user: User = req.user;
+        if (user.email) {
+          try {
+            const oldVerification = await Verification.findOne({
+              payload: user.email
+            });
+            if (oldVerification) {
+              oldVerification.remove();
+            }
+            const newVerification = await Verification.create({
+              payload: user.email,
+              target: "EMAIL"
+            }).save();
+            await sendVerificationEmail(user.fullName, newVerification.key);
+            return {
+              ok: true,
+              error: null
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message
+            };
+          }
+        }
+        return {
+          ok: false,
+          error: "Your user has no email to be verified"
+        };
+      }
+    )
+  }
+};
+
+export default resolvers;
