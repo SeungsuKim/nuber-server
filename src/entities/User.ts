96df97376699102004e533938e60ea52eadbb74f
seungsukim <seungsu0407@gmail.com>
2019-06-13 15:00:47 +0900

    #1.60 AddPlace Resolver

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -15,6 +15,7 @@ import {
 
 import Chat from "./Chat";
 import Message from "./Message";
+import Place from "./Place";
 import Ride from "./Ride";
 
 const BCRPYT_ROUNDS = 10;
@@ -84,6 +85,9 @@ class User extends BaseEntity {
 
   @OneToMany(type => Ride, ride => ride.driver)
   ridesAsDriver: Ride[];
+
+  @OneToMany(type => Place, place => place.user)
+  places: Place[];
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
