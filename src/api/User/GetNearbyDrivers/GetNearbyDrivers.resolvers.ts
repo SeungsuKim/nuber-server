9d146b75537f2e83b56a62d4c63b13b3016c806f
seungsukim <seungsu0407@gmail.com>
2019-06-13 17:17:31 +0900

    #1.64 GetNearbyDrivers Resolver

---
+++ src/api/User/GetNearbyDrivers/GetNearbyDrivers.resolvers.ts
@@ -0,0 +1,25 @@
+import { privateResolver } from "src/middlewares";
+import { GetNearbyDriversResponse } from "src/types/graphql";
+import { Resolvers } from "src/types/resolvers";
+import { Between } from "typeorm";
+
+import User from "../../../entities/User";
+import resolvers from "../ReportMovement/ReportMovement.resolvers";
+
+const resolvres: Resolvers = {
+  Query: {
+    GetNearbyDrivers: privateResolver(
+      async (_, __, { req }): Promise<GetNearbyDriversResponse> => {
+        const user: User = req.user;
+        const { lastLat, lastLng } = user;
+        const drivers = await User.find({
+          isDriving: true,
+          lastLat: Between(lastLat - 0.05, lastLat + 0.05),
+          lastLng: Between(lastLng - 0.05, lastLng + 0.05)
+        });
+      }
+    )
+  }
+};
+
+export default resolvers;
