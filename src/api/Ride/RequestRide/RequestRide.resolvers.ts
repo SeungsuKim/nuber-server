4fa79d602aee451611983047dbbbaacaae0dd6ca
seungsukim <seungsu0407@gmail.com>
2019-06-15 06:20:12 +0900

    #1.75 Testing the NearbyRideSubscription

--- src/api/Ride/RequestRide/RequestRide.resolvers.ts
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -12,21 +12,30 @@ const resolvers: Resolvers = {
       { req, pubSub }
     ): Promise<RequestRideResponse> => {
       const user: User = req.user;
-      try {
-        const ride = await Ride.create({ ...args, passenger: user }).save();
-        pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
-        return {
-          ok: true,
-          error: null,
-          ride
-        };
-      } catch (error) {
-        return {
-          ok: false,
-          error: error.message,
-          ride: null
-        };
+      if (!user.isRiding) {
+        try {
+          const ride = await Ride.create({ ...args, passenger: user }).save();
+          pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
+          user.isRiding = true;
+          user.save();
+          return {
+            ok: true,
+            error: null,
+            ride
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            ride: null
+          };
+        }
       }
+      return {
+        ok: false,
+        error: "You cannot request two rides",
+        ride: null
+      };
     }
   }
 };
